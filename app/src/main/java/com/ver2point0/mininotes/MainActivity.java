fefed620c6c2f1881adb06d3c6fb05dd16efcad9
John McIntosh II <jmci10@hotmail.com>
2015-08-06 18:27:03 -0400

    database progress

--- app/src/main/java/com/ver2point0/mininotes/MainActivity.java
+++ app/src/main/java/com/ver2point0/mininotes/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ver2point0.mininotes;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insertNote("New note");
+    }
+
+    private void insertNote(String noteText) {
+        ContentValues values = new ContentValues();
+        values.put(DBOpenHelper.NOTE_TEXT, noteText);
+        Uri noteUri = getContentResolver().insert(NotesProvider.CONTENT_URI,
+                values);
+        Log.d("MainActivity", "Inserted note " + noteUri.getLastPathSegment());
     }
 
     @Override
